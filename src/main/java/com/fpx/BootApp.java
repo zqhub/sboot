dc31460309cafe3eb7d5125b05746ff05a2b7ec9
unknown <zhengjianc@4pxad.com>
2019-07-11 15:37:47 +0800

    Initial commit

---
+++ src/main/java/com/fpx/BootApp.java
@@ -0,0 +1,11 @@
+package com.fpx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApp.class, args);
+    }
+}
